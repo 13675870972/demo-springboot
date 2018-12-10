82306773a2b68afaf91dc3ad76ac7774321a33e7
cyc <cay>
2018-12-10 09:50:54 +0800

    0

--- src/main/java/com/example/demospringboot/action/DemoSpringBootApplication.java
+++ src/main/java/com/example/demospringboot/action/DemoSpringBootApplication.java
@@ -1,14 +1,17 @@
-package com.example.demospringboot;
+package com.example.demospringboot.action;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+/**
+ * @author cyc
+ */
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-public class DemoSpringbootApplication {
+public class DemoSpringBootApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DemoSpringbootApplication.class, args);
+        SpringApplication.run(DemoSpringBootApplication.class, args);
     }
 }
