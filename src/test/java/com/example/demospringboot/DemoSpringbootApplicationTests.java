fe93055396d0e2cda6a61f1f1ea643a0ec3376e8
caoyc <414582939@qq.com>
2019-09-03 15:12:50 +0800

    00

--- src/test/java/com/example/demospringboot/DemoSpringbootApplicationTests.java
+++ src/test/java/com/example/demospringboot/DemoSpringbootApplicationTests.java
@@ -43,5 +43,6 @@ public class DemoSpringbootApplicationTests {
     @Test
     public void testDB() throws Exception {
         System.err.println("111");
+        System.err.println("111");
     }
 }
